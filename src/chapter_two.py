e62a9a3cf71c3455d9da558a1313e649479d44d8
macbook pro <firsov.ip@gmail.com>
2022-11-15 01:46:03 +0300

    2.3.1 done

--- src/chapter_two.py
+++ src/chapter_two.py
@@ -1,12 +1,27 @@
 import asyncio
+from util import delay
 
 
-async def delay(delay_seconds: int) -> int:
-    print(f'start sleeping {delay_seconds} sec.')
-    await asyncio.sleep(delay_seconds)
-    print(f'sleeping {delay_seconds} sec. done')
-    return delay_seconds
+async def add_one(number: int) -> int:
+    return number + 1
+
+
+async def hello_world_message() -> str:
+    await delay(1)
+    return 'Hello World!'
+
+
+async def main() -> None:
+    # message = await hello_world_message()
+    # one_plus_one = await add_one(1)
+    # print(one_plus_one)
+    # print(message)
+    # asyncio.run(main())
+    sleep_for_three = asyncio.create_task(delay(3))
+    print(type(sleep_for_three))
+    result = await sleep_for_three
+    print(result)
 
 
 if __name__ == '__main__':
-    pass
+    asyncio.run(main())
