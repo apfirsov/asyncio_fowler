200de1bc31108807ddcee331a29ebc8430e45ffc
macbook pro <firsov.ip@gmail.com>
2022-11-17 00:53:37 +0300

    2.4.1 done

--- src/chapter_two.py
+++ src/chapter_two.py
@@ -1,46 +1,22 @@
 import asyncio
+from asyncio import CancelledError
 from util import delay
 
 
-async def add_one(number: int) -> int:
-    return number + 1
-
-
-async def hello_world_message() -> str:
-    await delay(1)
-    return 'Hello World!'
-
-
-async def hello_every_second():
-    for i in range(15):
+async def main():
+    long_task = asyncio.create_task(delay(10))
+    seconds_elapsed = 0
+    while not long_task.done():
+        print('Задача не закончилась, следующая проверка через секунду.')
         await asyncio.sleep(1)
-        print("While i wait, do something")
+        seconds_elapsed = seconds_elapsed + 1
+        if seconds_elapsed == 5:
+            long_task.cancel()
+    try:
+        await long_task
+    except CancelledError:
+        print('Наша задача была снята')
 
-
-async def main() -> None:
-    # message = await hello_world_message()
-    # one_plus_one = await add_one(1)
-    # print(one_plus_one)
-    # print(message)
-    # asyncio.run(main())
-
-    # sleep_for_three = asyncio.create_task(delay(3))
-    # print(type(sleep_for_three))
-    # result = await sleep_for_three
-    # print(result)
-
-    # sleep_for_three = asyncio.create_task(delay(3))
-    # sleep_again = asyncio.create_task(delay(3))
-    # sleep_once_more = asyncio.create_task(delay(3))
-    # await sleep_for_three
-    # await sleep_again
-    # await sleep_once_more
-
-    first_delay = asyncio.create_task(delay(10))
-    second_delay = asyncio.create_task(delay(10))
-    await hello_every_second()
-    await first_delay
-    await second_delay
 
 if __name__ == '__main__':
     asyncio.run(main())
