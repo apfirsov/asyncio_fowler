915c420aa9805fce30d6d8f2f58a573d9cfaa177
macbook pro <firsov.ip@gmail.com>
2022-11-21 00:31:26 +0300

    2.4 solved

--- src/chapter_two.py
+++ src/chapter_two.py
@@ -3,7 +3,7 @@ from asyncio import CancelledError
 from util import delay
 
 
-async def main():
+async def main_cancel_task():
     long_task = asyncio.create_task(delay(10))
     seconds_elapsed = 0
     while not long_task.done():
@@ -18,5 +18,17 @@ async def main():
         print('Наша задача была снята')
 
 
+async def main_time_out():
+    delay_task = asyncio.create_task(delay(2))
+    try:
+        result = await asyncio.wait_for(delay_task, timeout=1)
+        print(result)
+    except asyncio.exceptions.TimeoutError:
+        print('Тайм-аут!')
+        print(f'Задача была снята? {delay_task.cancelled()}')
+
+
 if __name__ == '__main__':
-    asyncio.run(main())
+    # asyncio.run(main_cancel_task())
+    asyncio.run(main_time_out())
+
